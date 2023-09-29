757f56b4eb02b879c5362ffb2261182a2e805d69
Joonsung-Byun <122327073+Joonsung-Byun@users.noreply.github.com>
2023-09-29 03:06:20 -0600

    Finish

---
+++ data/reviews.js
@@ -0,0 +1,28 @@
+export const reviews = [
+    {
+      "name": "James Smith",
+      "stars": 4,
+      "text": "I have been a proud owner of a Chevrolet vehicle for several years now, and I must say that the fuel efficiency of these cars is truly exceptional. Whether I'm taking a long road trip or just commuting to work, I consistently notice how efficient my Chevrolet is. It's a great feeling knowing that I'm not only getting where I need to go but also saving money on fuel costs. The performance, especially on the highway, is impressive, and I can confidently say that Chevrolet cars are a fantastic choice for anyone looking for a fuel-efficient and reliable vehicle."
+    },
+    {
+      "name": "Mary Johnson",
+      "stars": 5,
+      "text": "Choosing Chevrolet for my latest vehicle purchase was one of the best decisions I've ever made. The safety features in these cars are truly top-notch. As a parent, safety is my utmost concern, and Chevrolet's commitment to safety gave me peace of mind. Moreover, the sleek and stylish design of my Chevrolet car exceeded all my expectations. It's not just a mode of transportation; it's a statement of style. From the moment I laid eyes on it, I knew I had made the right choice. Chevrolet has not only provided me with a safe and reliable vehicle but also a sense of pride and satisfaction that I carry with me on every drive."
+    },
+    {
+      "name": "John Williams",
+      "stars": 3,
+      "text": "My experience with Chevrolet cars has been generally positive. I find that they offer good value for the price you pay. They are well-built and provide a comfortable driving experience. However, one aspect that could use some improvement is the navigation system. At times, it feels a bit slow and clunky to use. Given the advancements in technology today, I believe Chevrolet can enhance this aspect of their vehicles to match the overall quality they offer. Despite this minor drawback, I still consider Chevrolet to be a solid choice in the automotive market."
+    },
+    {
+      "name": "Jennifer Brown",
+      "stars": 5,
+      "text": "I cannot praise Chevrolet's service quality enough. Every time I've taken my Chevrolet in for scheduled maintenance or any other service, I've received exceptional treatment. The staff is not only professional but also genuinely friendly. They take the time to explain everything to me, and I always feel like a valued customer. The promptness of their service is also worth noting. I've never had to wait long, and that's something I truly appreciate. Owning a Chevrolet isn't just about the car itself; it's also about the outstanding service and support that comes with it. Chevrolet has earned my trust and loyalty through their commitment to excellence."
+    },
+    {
+      "name": "Michael Jones",
+      "stars": 4,
+      "text": "When I decided to purchase a Chevrolet, one of the primary reasons was the spacious interior. I'm a tall guy, and I've always had trouble finding a car with enough legroom and headroom to be comfortable on long journeys. My Chevrolet has exceeded my expectations in this regard. It offers plenty of room for both the driver and passengers, making every trip a pleasure. Whether it's a family vacation or just a daily commute, I never feel cramped or uncomfortable. The spacious interior of my Chevrolet has made it a joy to own and drive."
+    }
+  ]
+  
---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+import { reviews } from "../data/reviews.js";
+console.log(reviews); // Import as Array form
+
+const myTarget = document.querySelector("#cards")
+
+for (let x =0; x < reviews.length; x++) {
+    const mySection = document.createElement('section')
+    const myName = document.createElement('h3')
+    myName.textContent = reviews[x].name;
+    const myReview = document.createElement('p')
+    myReview.textContent = reviews[x].text;        //5번반복해서 이름이랑 후기 다 넣었음
+
+    const myRating = document.createElement('div') //별 넣을 div 생성
+    
+
+    console.log(reviews[x].stars) // Each reviewers' stars number
+    for (let step = 0; step < reviews[x].stars; step++) {
+        const genIndicator = document.createElement('img') //이 createElement를 reviews[x].stars만큼 한다는거. 즉, 요소 생성을 해당 숫자만큼 반복하겠다.
+        genIndicator.src = "../images/Blue-Star.svg"
+        myRating.appendChild(genIndicator)
+    }
+    
+    mySection.appendChild(myName)
+    mySection.appendChild(myRating)
+    mySection.appendChild(myReview)
+    myTarget.appendChild(mySection)
+}
+
+const rightNow = new Date()
+document.querySelector('#year').textContent = rightNow.getFullYear();
+
+function toggleMenu() {
+    document.querySelector('#btn').classList.toggle("open")
+    document.querySelector('#primaryNav').classList.toggle("open")
+}
+
+const x = document.querySelector('#btn')
+x.onclick = toggleMenu;
